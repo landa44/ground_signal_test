46296d285eaf54286c3cb578bc9ff439bcb97486
landa44 <pablo44landa@gmail.com>
2023-10-19 16:56:31 -0400

    updating project structure

--- src/features/Map/Map.js
+++ src/features/Map/Map.js
@@ -1,16 +1,10 @@
 import {React, useEffect} from 'react';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
-import 'leaflet-defaulticon-compatibility';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import { selectFilteredLocations } from '../Location/locationsSlice';
 import { useSelector } from 'react-redux';
 import Leaflet from 'leaflet';
 
-// const customMapStyle = {
-//   width: '100%',
-//   height: '100vh'
-// };
 
 export default function Map({children, className, center}) {
   const locations = useSelector(selectFilteredLocations);
