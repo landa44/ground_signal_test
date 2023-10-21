865f2f78cc5dc3017f12f4a4ea82ac8cc6c33417
landa44 <pablo44landa@gmail.com>
2023-10-21 18:37:26 -0400

    adding auto relocation when select a location in the searchBar

--- src/features/Map/CustomMap.js
+++ src/features/Map/CustomMap.js
@@ -1,6 +1,7 @@
 import React from "react";
 import dynamic from 'next/dynamic';
 
+
 const Map = dynamic(() => import('./Map'), {
     ssr: false
 });
@@ -8,34 +9,32 @@ const Map = dynamic(() => import('./Map'), {
 export default function CustomMap({className, center, locations, selectedLocation, handlerMarkerClick}) {
   return (
     <Map className={className} center={center}>
-      { (TileLayer, Marker, Leaflet) =>
+      { (TileLayer, Marker, SelectedLocationMarker, Leaflet) =>
         <>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           />
           {
-            locations.map( item =>
+            locations.map( item => selectedLocation === null || selectedLocation.id != item.id?
               <Marker 
                 key={item.id} 
                 position={[item.location.lat, item.location.lon]} 
                 icon={Leaflet.icon(
-                  selectedLocation === null || selectedLocation.id != item.id?
                   {
                     iconUrl: 'leaflet/images/marker-icon.png',
                     iconRetinaUrl: 'leaflet/images/marker-icon-2x.png', 
-                    iconSize:[25, 41], 
-                  }:
-                  {
-                    iconUrl: 'leaflet/images/marker-icon-gold.png',
-                    iconRetinaUrl: 'leaflet/images/marker-icon-2x-gold.png', 
                     iconSize:[25, 41], 
                   }
                 )}
                 eventHandlers={{
                   click: () => handlerMarkerClick(item),
                 }}
-              
+              />:
+              <SelectedLocationMarker
+                key={item.id} 
+                position={[item.location.lat, item.location.lon]}
+                handlerMarkerClick={() => handlerMarkerClick(item)} 
               />
             )
           }
